e12056bca4739bbf0a541ebcba1c9ac59c0fe8bc
arnabb38 <arnabbasak896@gmail.com>
2020-10-07 23:08:58 +0600

    quick fix

--- live-score.py
+++ live-score.py
@@ -8,4 +8,11 @@ matches = c.matches()
 # for match in matches:
 #     print(match)
 
-print (json.dumps(matches,indent=4))
+# print (json.dumps(matches,indent=4))
+
+def scorecard(mid):
+    c = Cricbuzz()
+    scard = c.scorecard(mid)
+    print(json.dumps(scard, indent=4, sort_keys=True))
+
+scorecard()
