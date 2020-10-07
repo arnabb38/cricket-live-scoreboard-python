5f7d2ca7cb12fea4c5a8813f4726937099c2defe
arnabb38 <arnabbasak896@gmail.com>
2020-10-07 23:42:37 +0600

    added scorecard

--- live-score.py
+++ live-score.py
@@ -8,11 +8,18 @@ matches = c.matches()
 # for match in matches:
 #     print(match)
 
-# print (json.dumps(matches,indent=4))
+# print (json.dumps(matches,indent=4)) # 30415
 
 def scorecard(mid):
     c = Cricbuzz()
     scard = c.scorecard(mid)
     print(json.dumps(scard, indent=4, sort_keys=True))
 
-scorecard()
+scorecard("30415")
+
+# def match_info(mid):
+#     # c = Cricbuzz()
+#     minfo = c.matchinfo(mid)
+#     print(json.dumps(minfo, indent=4, sort_keys=True))
+
+# match_info("30415")
